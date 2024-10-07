0dca22231369dd2f363417e96d0a0e6d36c42bf9
곽윤호 <kwakdm95@gmail.com>
2024-10-07 17:03:59 +0900

    Index 페이지 제작

--- charlie/views.py
+++ charlie/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render, HttpResponse
 
+topics = [
+  {"id": 1, "title": "create", "body": "Hello Create"},
+  {"id": 2, "title": "read", "body": "Hello Read"},
+  {"id": 3, "title": "update", "body": "Hello Update"},
+  {"id": 4, "title": "delete", "body": "Hello Delete"}, 
+]
+
 def index(request):
-  return HttpResponse("Hello World")
+  global topics
+  lists = ""
+  for topic in topics:
+    lists += f"<li style='text-transform:capitalize;'><a href='/read/{topic['id']}'>{topic['title']}</a></li>"
+  return HttpResponse(f"""
+  <html>
+  <body>
+    <h1>Welcome to Charlie</h1>
+    <ol>
+      {lists}
+    </ol>
+  </body>
+  </html>
+""")
 
 def create(request):
   return HttpResponse("Create Section")
