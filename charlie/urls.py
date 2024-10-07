d66a4666d2df00ad91d7e2d2235152a59801cf8e
곽윤호 <kwakdm95@gmail.com>
2024-10-07 19:50:20 +0900

    CRUD 기능과 Routing 처리

--- charlie/urls.py
+++ charlie/urls.py
@@ -4,5 +4,7 @@ from charlie import views
 urlpatterns = [
     path('', views.index),
     path('create/', views.create),
-    path('read/<int:id>', views.read)
+    path('read/<int:id>', views.read),
+    path('delete/<int:id>', views.delete),
+    path('update/<int:id>', views.update)
 ]
--- charlie/views.py
+++ charlie/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.views.decorators.csrf import csrf_exempt
 
 topics = [
   {"id": 1, "title": "create", "body": "Hello Create"},
@@ -6,6 +7,7 @@ topics = [
   {"id": 3, "title": "update", "body": "Hello Update"},
   {"id": 4, "title": "delete", "body": "Hello Delete"}, 
 ]
+nextId = 5
 
 def HTMLLayout(children):
   global topics
@@ -24,6 +26,12 @@ def HTMLLayout(children):
         </nav>
       </header>
       {children}
+      <footer>
+        <h3>Features</h3>
+        <ul>
+          <li><a href="/create">Create</a></li>
+        </ul>
+      </footer>
     </body>
   </html>
   """
@@ -48,9 +56,81 @@ def read(request, id):
   main = f"""
   {title}
   {article}
+  <ul>
+    <li>
+      <a href="/update/{id}">
+        <button>update article</button>
+      </a>
+    </li>
+    <li>
+      <a href="/delete/{id}">
+        <button>delete article</button>
+      </a>
+    </li>
+  </ul>
   """
   return HttpResponse(HTMLLayout(main))
 
+@csrf_exempt
 def create(request):
+  if request.method == "GET":
+    main = f"""
+    <form action="/create/" method="post">
+      <div>
+        <label>Title</label>
+        <p><input type="text" name="title"></p>
+      </div>
+      <div>
+        <label>Body</label>
+        <p><textarea name="body"></textarea></p>
+      </div>
+      <input type="submit">
+    </form>
+    """
+    return HttpResponse(HTMLLayout(main))
+  elif request.method == "POST":
+    global nextId
+    global topics
+    newTopic = {"id": nextId, "title": request.POST["title"], "body": request.POST["body"]}
+    redirectUrl = f"/read/{newTopic['id']}"
+    topics.append(newTopic)
+    nextId = nextId + 1
+    return redirect(redirectUrl)
   
-  return HttpResponse("Create Section")
+def delete(_, id):
+  global topics
+  for topic in topics:
+    if topic["id"] == id:
+      topics.remove(topic)
+      break
+  return redirect("/")
+
+@csrf_exempt
+def update(request, id):
+  global topics
+  if request.method == "GET":
+    main = ""
+    for topic in topics:
+      if topic["id"] == int(id):
+        main = f"""
+        <form action="/update/{id}" method="post">
+          <div>
+            <label>Title</label>
+            <p><input type="text" name="title" value={topic["title"]}></p>
+          </div>
+          <div>
+            <label>Body</label>
+            <p><textarea name="body">{topic["body"]}</textarea></p>
+          </div>
+          <input type="submit">
+        </form>
+        """
+        return HttpResponse(HTMLLayout(main))
+  elif request.method == "POST":
+    for topic in topics:
+      if topic["id"] == int(id):
+        topic["title"] = request.POST["title"]
+        topic["body"] = request.POST["body"]
+        break
+    redirectUrl = f"/read/{id}"
+    return redirect(redirectUrl)
